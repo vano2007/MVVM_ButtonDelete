bb334079d57af0b87f4f30d4901d3d863266e73d
Eduard Angelskiy <ed_angel74@mail.ru>
2023-05-16 18:50:42 +0700

    Commit_160523

--- app/src/main/java/com/example/mvvm_buttondelete/view/MainActivity2.java
+++ app/src/main/java/com/example/mvvm_buttondelete/view/MainActivity2.java
@@ -1,13 +1,18 @@
 package com.example.mvvm_buttondelete.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.widget.Adapter;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.mvvm_buttondelete.R;
 import com.example.mvvm_buttondelete.model.Notebook;
+import com.example.mvvm_buttondelete.viewmodel.Adapter;
 import com.example.mvvm_buttondelete.viewmodel.DatabaseHelper;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -39,5 +44,45 @@ public class MainActivity2 extends AppCompatActivity {
 
         notesList = new ArrayList<>(); // выделение памяти и задание типа контейнера для списка заметок
         database = new DatabaseHelper(this); // выделение памяти и задание текущего контекста работы с БД
+
+        // считывание данных из БД и запись их в коллекцию notesList
+        fetchAllNotes();
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this)); // задание структуры вывода данных в recyclerView
+        adapter = new Adapter(this, MainActivity2.this, notesList); // инициализация адаптера и передача в рего данных из БД
+        recyclerView.setAdapter(adapter); // передача в recyclerView адаптер
+
+        // обработка нажатия кнопки создания новой заметки
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // переключение на новую активность
+                startActivity(new Intent(MainActivity2.this, AddNotesActivity.class));
+            }
+        });
+        // обработка нажатия кнопки удаления всех заметок
+        fabDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // удаления записей
+                database.deleteAllNotes();
+                Toast.makeText(MainActivity2.this, "Данные из блокнота удалены", Toast.LENGTH_SHORT).show()
+            }
+        });
+    }
+
+    // метод считывания из БД всех записей
+    public void fetchAllNotes(){
+        // чтение БД и запись данных в курсор
+        Cursor cursor = database.readNotes();
+
+        if (cursor.getCount() == 0) { // если данные отсутствую, то вывод на экран об этом тоста
+            Toast.makeText(this, "Заметок нет", Toast.LENGTH_SHORT).show();
+        } else { // иначе помещение их в контейнер данных notesList
+            while (cursor.moveToNext()){
+                // помещение в контейнер notesList из курсора данных
+                notesList.add(new Notebook(cursor.getString(0), cursor.getString(1), cursor.getString(2)));
+            }
+        }
     }
 }
